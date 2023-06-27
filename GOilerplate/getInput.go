0e1d44e2d5c0152017fe386749bfceab03412cc6
Kai van Dalen <kaid@topdesk.com>
2023-06-27 13:48:29 +0200

    Boyscouting: Move input getter away from main

---
+++ GOilerplate/getInput.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"embed"
+	"io/fs"
+	"log"
+)
+
+//go:embed test_input.txt
+//go:embed input.txt
+var filesystem embed.FS
+
+func getInputFile(fileName string) fs.File {
+	// Read file, log error if something breaks
+	f, err := filesystem.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return f
+}
+
+func getInput(fileName string) []string {
+	var lines []string
+	scanner := bufio.NewScanner(getInputFile(fileName))
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
+}
--- GOilerplate/main.go
+++ GOilerplate/main.go
@@ -1,31 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"embed"
 	"fmt"
-	"io/fs"
-	"log"
 )
 
-//go:embed test_input.txt
-//go:embed input.txt
-var filesystem embed.FS
-
-func getInput() fs.File {
-	// Read file, log error if breakage
-	f, err := filesystem.Open("input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return f
-}
-
 func main() {
-	scanner := bufio.NewScanner(getInput())
-	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
-	}
+	inputFilename := "input.txt"
+	input := getInput(inputFilename)
+	fmt.Print(input)
 }
